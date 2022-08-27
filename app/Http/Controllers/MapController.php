bdda71c02636ac203197178b64752fe3de9cc954
Akash Kumar Verma <36815360+ultimateakash@users.noreply.github.com>
2022-08-27 18:24:18 +0530

    Add Leaflet Map

---
+++ app/Http/Controllers/MapController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MapController extends Controller
+{
+    public function index()
+    {
+        $initialMarkers = [
+            [
+                'position' => [
+                    'lat' => 28.625485,
+                    'lng' => 79.821091
+                ],
+                'draggable' => true
+            ],
+            [
+                'position' => [
+                    'lat' => 28.625293,
+                    'lng' => 79.817926
+                ],
+                'draggable' => false
+            ],
+            [
+                'position' => [
+                    'lat' => 28.625182,
+                    'lng' => 79.81464
+                ],
+                'draggable' => true
+            ]
+        ];
+        return view('map', compact('initialMarkers'));
+    }
+}
